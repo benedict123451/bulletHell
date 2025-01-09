1b0a3007e6f785a4166392855457948fd0e05fcd
benedict123451 <118616881+benedict123451@users.noreply.github.com>
2025-01-09 16:38:01 +0100

    fenetre visible

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <SFML/Window.hpp>
-
+#include <SFML/Graphics.hpp>
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
-
-    ...
-
-        return 0;
+// Largeur et hauteur de la fenêtre
+int widthWindow = 800, heightWindow = 600;
+// Création d'une fenêtre "render"
+sf::RenderWindow window(
+	sf::VideoMode(widthWindow, heightWindow),
+	"Titre de la fenêtre",
+	sf::Style::Default); // Pour un style personnalisé
+// Boucle principale avec gestion frame par frame
+while (window.isOpen()) {
+	// Gestion des évènements
+	sf::Event event;
+	while (window.pollEvent(event)) {
+		// Demande de fermeture (croix de la fenêtre, ALT+F4)
+		if (event.type == sf::Event::Closed)
+			window.close();
+		// D'autres évènements peuvent être gérés ici
+	}
+	// Effacement de l'ancienne frame (framebuffer)
+	window.clear(sf::Color::Black);
+	// ... Ici on dessine les objets, sprites, textes, formes
+	// Affiche la nouvelle frame à l'écran
+	window.display();
+}
 }
