c9c098c7c64da91ee639c5578076e229d23ca077
benedict123451 <118616881+benedict123451@users.noreply.github.com>
2025-01-10 09:09:57 +0100

    fenetre visible + debut cercle

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include "worldcomponent.hpp"
 int main()
 {
+	Motion motion = {
+		.direction_normalized = {1.f,0.f},
+		.speed = 10.0f
+	};
+
+	Transform transform = {
+		.position = {0.f,0.f}
+	};
+
+	sf::CircleShape shape(50.0f);
+	shape.setFillColor(sf::Color::Red);
+	shape.setPosition(10.f, 50.f);
+
 // Largeur et hauteur de la fenêtre
 int widthWindow = 800, heightWindow = 600;
 // Création d'une fenêtre "render"
@@ -10,7 +24,7 @@ sf::RenderWindow window(
 	"Titre de la fenêtre",
 	sf::Style::Default); // Pour un style personnalisé
 // Boucle principale avec gestion frame par frame
-while (window.isOpen()) {
+while(window.isOpen()) {
 	// Gestion des évènements
 	sf::Event event;
 	while (window.pollEvent(event)) {
@@ -19,8 +33,15 @@ while (window.isOpen()) {
 			window.close();
 		// D'autres évènements peuvent être gérés ici
 	}
+
+	update_position(motion, transform, 0.016f);
+
+
 	// Effacement de l'ancienne frame (framebuffer)
 	window.clear(sf::Color::Black);
+	sf::CircleShape shape(50.0f);
+	shape.setFillColor(sf::Color::Red);
+	shape.setPosition(10.f, 50.f);
 	// ... Ici on dessine les objets, sprites, textes, formes
 	// Affiche la nouvelle frame à l'écran
 	window.display();
